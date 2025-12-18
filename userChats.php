36023e564bfbec11a8bb3ecfc0765b5afc575b98
Eivind-HB <eivind.h.breivik@gmail.com>
2025-12-19 00:01:00 +0100

    Fjernet unødvendig kommentar

--- userChats.php
+++ userChats.php
@@ -6,7 +6,6 @@
     require_once __DIR__ . '/api/booksAPI.php';
     require_once __DIR__ . '/scripts/sessionStart.php';
     require_once __DIR__ . '/scripts/DB/db.inc.php';
-    //require_once __DIR__ . '/scripts/printChatlog.php';
     require_once __DIR__ . '/scripts/checkLoginStatus.php';    
     require_once __DIR__ . '/classes/ChatManager.php';
 
